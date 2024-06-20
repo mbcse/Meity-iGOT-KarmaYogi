cc6428bdf2fe35ea8cb7e85f7537a46d0b191f0e
shivam-jainn <shivam.clgstash@gmail.com>
2024-06-20 08:14:03 +0530

    added pills

--- karmayogi/src/app/channels/page.tsx
+++ karmayogi/src/app/channels/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ChatBubble from '@/components/atoms/ChatBubble'
+import Pill from '@/components/atoms/Pill'
+import { Button } from '@/components/ui/button'
 export default function page() {
   return (
+    <>
+    <Pill  label='Whatsapp' />
     <ChatBubble message='hello world' pfplink='https://images.pexels.com/photos/21050507/pexels-photo-21050507/free-photo-of-a-woman-with-an-umbrella-and-a-black-bag.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' />
+    </>
   )
 }
