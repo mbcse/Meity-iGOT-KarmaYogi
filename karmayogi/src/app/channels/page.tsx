503cc8de077b4cf883592bfb14a8d47981b53a3d
shivam-jainn <shivam.clgstash@gmail.com>
2024-06-20 08:04:55 +0530

    Created chat bubble component

---
+++ karmayogi/src/app/channels/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ChatBubble from '@/components/atoms/ChatBubble'
+export default function page() {
+  return (
+    <ChatBubble message='hello world' pfplink='https://images.pexels.com/photos/21050507/pexels-photo-21050507/free-photo-of-a-woman-with-an-umbrella-and-a-black-bag.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' />
+  )
+}
