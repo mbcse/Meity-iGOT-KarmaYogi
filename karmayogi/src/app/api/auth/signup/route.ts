77d38fa12b2a5b425fc511c9a605d5eb0f30c13d
shivam-jainn <shivam.clgstash@gmail.com>
2024-06-25 08:01:40 +0530

    added auth apis , setup auth db, docker compose added

---
+++ karmayogi/src/app/api/auth/signup/route.ts
@@ -0,0 +1,49 @@
+import { PrismaClient } from "@prisma/client/extension";
+import { NextRequest, NextResponse } from "next/server";
+import {hash} from '@node-rs/argon2'
+import jwt from 'jsonwebtoken'
+
+export async function POST(req:NextRequest,res:NextResponse){
+    try {
+        const prisma = new PrismaClient();
+        const {email,password,name} = await req.json();
+
+        const passwordHash = await hash(password, {
+            memoryCost: 19456,
+            timeCost: 2,
+            outputLen: 32,
+            parallelism: 1
+        });
+
+        const user = prisma.user.create({
+            data:{
+                email:email,
+                name : name,
+                password:passwordHash
+            }
+        })
+
+        if(!user){
+            return NextResponse.json({
+                type : "error",
+                message : "User could not be created ."
+            })
+        }
+
+
+        const token = jwt.sign({
+            id : user.id,
+            email:user.email
+        },process.env.PASS_HASH_KEY as string,{
+            expiresIn:'2d'
+        });
+
+        return NextResponse.json({
+            type:"success",
+            message : token
+        });
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
