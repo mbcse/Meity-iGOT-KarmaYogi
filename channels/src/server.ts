25551760377b8801e4952ccc7fb634326046e264
shivam-jainn <shivam.clgstash@gmail.com>
2024-08-18 22:59:11 +0530

    init channels server

---
+++ channels/src/server.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response, NextFunction } from 'express';
+import mongoose from 'mongoose';
+import winston from 'winston';
+import connectDB from './config/database';
+
+const app = express();
+
+// Logger setup with Winston
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+// Middleware
+app.use(express.json());
+
+connectDB();
+
+// Error handling
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err); // Logs the error using the Winston logger
+  res.status(500).send({ error: err.message }); // Sends a 500 Internal Server Error response with the error message
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  logger.info(`Server running on port ${port}`);
+});
