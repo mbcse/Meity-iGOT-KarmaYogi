8d0d36b7f6a7a4f242ccb0d8a048cf4f729d6c9e
shivam-jainn <shivam.clgstash@gmail.com>
2024-09-01 13:40:39 +0530

    (HOTFIX) removed backtick from &quot in overallpie.tsx

--- karmayogi/src/app/((main))/reports/[...campaignid]/components/OverallPie.tsx
+++ karmayogi/src/app/((main))/reports/[...campaignid]/components/OverallPie.tsx
@@ -65,7 +65,7 @@ return (
     <Card className="flex flex-col min-w-[400px] w-full">
         <CardHeader className="items-center pb-0">
             <CardTitle>Radial Chart - Grid</CardTitle>
-            <CardDescription>Statistics for `&quot;`20% off`&quot;` campaign</CardDescription>
+            <CardDescription>Statistics for &quot;20% off&quot; campaign</CardDescription>
         </CardHeader>
         <CardContent className="flex-1 pb-0">
             <ChartContainer
